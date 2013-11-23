02039f8d5fe383ee3f047fc21543dfb2b799d48d
Quevedo Díaz <Quevedo Daz@laptopqd>
2013-11-23 14:53:33 -0500

    Inicio de sesión de usuarios.

--- inventarios/src/inventarios/Mundo/Usuario.java
+++ inventarios/src/inventarios/Mundo/Usuario.java
@@ -1,18 +1,49 @@
 package inventarios.Mundo;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class Usuario {
 
-	private String usuario;
-	private String clave;
+    private String usuario;
+    private String clave;
 
-	/**
-	 * 
-	 * @param usuario
-	 * @param clave
-	 */
-	public boolean login(String usuario, String clave) {
-		// TODO - implement Usuario.login
-		throw new UnsupportedOperationException();
-	}
+    public String getUsuario() {
+        return this.usuario;
+    }
 
+    public String getClave() {
+        return this.clave;
+    }
+
+    public Usuario() {
+    }
+
+    public Usuario(String usuario, String clave) {
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    /**
+     *
+     * @param usuario
+     * @param clave
+     */
+    public boolean login(String usuario, char[] clave, ArrayList<Usuario> usuarios) {
+        String clavetxt = new String(clave);
+        boolean loggedin = false;
+        for (Usuario user : usuarios) {
+            if (usuario.equals(user.getUsuario()) && clavetxt.equals(user.getClave())) {
+                loggedin = true;
+                break;
+            }
+        }
+        if (loggedin) {
+            JOptionPane.showMessageDialog(null, "BIENVENIDO " + usuario, "Login", JOptionPane.INFORMATION_MESSAGE);
+            return true;
+        } else {
+            JOptionPane.showMessageDialog(null, "Usuario y/o clave incorrecto(s)", "ERROR", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
 }
